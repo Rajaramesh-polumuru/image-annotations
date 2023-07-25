27bf72448e766cebaa33b6f43793198f5f31b8ee
rajaramesh-polumuru <rajaramesh.polumuru@gmail.com>
2023-07-25 16:53:22 +0530

    Add Background image component

---
+++ src/components/BackgroundImage.tsx
@@ -0,0 +1,30 @@
+import { memo } from "react";
+import { Image } from "react-konva";
+import useImage from "use-image";
+import { images } from "../dummy-data/imageData";
+
+const BackgroundImage = memo(({ imageIndex }: { imageIndex: number }) => {
+ 
+  const [image] = useImage(images[imageIndex]);
+
+  if (!image) return null;
+
+  const scaleWidth = 600 / image.width;
+  const scaleHeight = 600 / image.height;
+  const scale = Math.min(scaleWidth, scaleHeight);
+
+  const x = 0;
+  const y = 0;
+
+  return (
+    <Image
+      image={image}
+      width={image.width * scale}
+      height={image.height * scale}
+      x={x}
+      y={y}
+    />
+  );
+});
+
+export default BackgroundImage;
